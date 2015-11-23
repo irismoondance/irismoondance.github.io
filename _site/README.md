d71333af37d110d53d10b040e8b137089ab91c89
Dave Raskin <draskin1991@gmail.com>
2015-11-23 16:43:17 -0500

    added rudimentary toggle

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# irismoondance.github.io
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+var toggleSidebar = function() {
+  console.log('yeah, this happened.')
+  $('#wrapper').toggleClass('toggled');
+  $('#sidebar').toggleClass('toggled');
+}
